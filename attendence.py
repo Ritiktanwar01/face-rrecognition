624f9ed84c78836b74943f9e5d85af4e77c602ec
ritik337 <59756227+ritik337@users.noreply.github.com>
2025-02-03 19:49:35 +0530

    Last commit

--- attendence.py
+++ attendence.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import face_recognition
 import os
+from datetime import datetime,timezone,timedelta
 
 
 path = 'Images'
@@ -13,6 +14,48 @@ classNames = []
 myList = os.listdir(path)
 
 
+
+def getDateTime():
+    tz = timezone(timedelta(hours=5,minutes=30))
+    now = datetime.now(tz)
+    date = now.strftime("%d")
+    month = now.strftime("%m")
+    year = now.strftime("%Y")
+    hour = now.strftime("%I")
+    minute = now.strftime("%M")
+    am_pm = now.strftime("%p").upper()
+
+    month_dict = {
+        "01":"Jan",
+        "02":"Feb",
+        "03":"Mar",
+        "04":"Apr",
+        "05":"May",
+        "06":"Jun",
+        "07":"Jul",
+        "08":"Aug",
+        "09":"Sep",
+        "10":"Oct",
+        "11":"Nov",
+        "12":"Dec"
+    }
+
+    month_name = month_dict.get(month, "Invalid month")
+
+    formated_date_Time  = f"{date}--{month_name.upper()}-{year} {hour}:{minute} {am_pm}"
+    return formated_date_Time
+
+
+def mark_attendence(name):
+    with open('attendence.csv','r+') as f:
+        my_data_List = f.readlines()
+        name_List = []
+        for line in my_data_List:
+            entry = line.split(",")
+            name_List.append(entry[0])
+        if name not in name_List:
+            date_String = getDateTime()
+            f.write(f'\n{name},{date_String}')
 
 for cl in myList:
     curImg = cv2.imread(f"{path}/{cl}")
@@ -34,17 +77,32 @@ encode_list_known = find_encodings(image_list)
 print("encoding complete")
 
 
-cap = cv2.VideoCapture(1)
 
 
-while True:
-    success, img = cap.read()
-    img_small = cv2.resize(img,(0,0),None,0.25,0.25)
-    img_small = cv2.cvtColor(img_small,cv2.COLOR_BGR2RGB)
-    faces_current_frame = face_recognition.face_locations(img_small)
-    encoding_current_frame_cam = face_recognition.face_encodings(img_small,faces_current_frame)
+cap = cv2.VideoCapture(0)
 
-    for encodeFace,faceLoc in zip(encoding_current_frame_cam,faces_current_frame):
-        matches = face_recognition.compare_faces(encode_list_known,encodeFace)
-        faceDis = face_recognition.face_distance(encode_list_known,encodeFace)
-        print(faceDis)
+def attendence_system ():
+    while True:
+        success, img = cap.read()
+        img_small = cv2.resize(img,(0,0),None,0.25,0.25)
+        img_small = cv2.cvtColor(img_small,cv2.COLOR_BGR2RGB)
+        faces_current_frame = face_recognition.face_locations(img_small)
+        encoding_current_frame_cam = face_recognition.face_encodings(img_small,faces_current_frame)
+
+        for encodeFace,faceLoc in zip(encoding_current_frame_cam,faces_current_frame):
+            matches = face_recognition.compare_faces(encode_list_known,encodeFace)
+            faceDis = face_recognition.face_distance(encode_list_known,encodeFace)
+            # print(faceDis)
+            matchIndex = np.argmin(faceDis)
+
+
+            if matches[matchIndex]:
+                name = classNames[matchIndex].upper()
+                mark_attendence(name)
+                y1,x2,y2,x1 = faceLoc
+                y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
+                cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+                cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+                cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+        cv2.imshow('webcam',img)
+        cv2.waitKey(1)
---
+++ main.py
@@ -0,0 +1,45 @@
+from attendence import attendence_system
+import os
+import cv2
+import time
+
+
+
+def Register_Face():
+    cap = cv2.VideoCapture(0)
+    Student_name = str(input("Enter Student name : "))
+    print("please dont shake while taking picture the picture will be clicked in 8 next seconds try to look straight into your webcam \n and also please try to be close to the camera so that system could fetch your face then press 'C' key to capture")
+    time.sleep(8)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        cv2.imshow('Webcam', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('c'):
+            cv2.imwrite(os.path.join('Images', f'{Student_name}.jpg'), frame)
+            print("Success")
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
+def main():
+    print("Press 1 and then 'enter key' to register a student face id \n Press 2 to start marking attendence for today")
+    user_input = int(input("Enter your choice : "))
+
+
+    if (user_input == 1):
+        Register_Face()
+    elif (user_input == 2) :
+        print("activating attendence system")
+        attendence_system()
+    else:
+        print("Wrong Choice")
+
+
+while True:
+    main()
